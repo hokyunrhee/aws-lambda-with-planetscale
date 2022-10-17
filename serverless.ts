f5797669e6eddad9e3e10a791a8676c9b8e15a58
hokyunrhee <hokyun.rhee@gmail.com>
2022-10-17 23:38:51 +0900

    chore: 사용하지 않는 스크립트 제거

--- serverless.ts
+++ serverless.ts
@@ -1,14 +1,14 @@
 import type { AWS } from "@serverless/typescript";
 
-import hello from "@functions/hello";
-
 const serverlessConfiguration: AWS = {
   service: "aws-lambda-with-planetscale-and-prisma",
   frameworkVersion: "3",
   plugins: ["serverless-esbuild"],
+  useDotenv: true,
   provider: {
     name: "aws",
     runtime: "nodejs14.x",
+    region: "ap-northeast-2",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
@@ -16,10 +16,10 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
       NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
+      DATABASE_URL: "${env:DATABASE_URL}",
     },
   },
-  // import the function via paths
-  functions: { hello },
+  functions: {},
   package: {
     individually: true,
     patterns: [
