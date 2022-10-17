d251c185134e4104339e2c84e4d772fca876a916
hokyunrhee <hokyun.rhee@gmail.com>
2022-10-17 23:30:18 +0900

    build: prisma 추가

--- serverless.ts
+++ serverless.ts
@@ -1,35 +1,44 @@
-import type { AWS } from '@serverless/typescript';
+import type { AWS } from "@serverless/typescript";
 
-import hello from '@functions/hello';
+import hello from "@functions/hello";
 
 const serverlessConfiguration: AWS = {
-  service: 'aws-lambda-with-planetscale-and-prisma',
-  frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  service: "aws-lambda-with-planetscale-and-prisma",
+  frameworkVersion: "3",
+  plugins: ["serverless-esbuild"],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
+    name: "aws",
+    runtime: "nodejs14.x",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
     },
   },
   // import the function via paths
   functions: { hello },
-  package: { individually: true },
+  package: {
+    individually: true,
+    patterns: [
+      "src/generated/client/schema.prisma",
+      "!src/generated/client/libquery_engine-*",
+      "src/generated/client/libquery_engine-rhel-*",
+      "!node_modules/prisma/libquery_engine-*",
+      "!node_modules/@prisma/engines/**",
+    ],
+  },
   custom: {
     esbuild: {
       bundle: true,
       minify: false,
       sourcemap: true,
-      exclude: ['aws-sdk'],
-      target: 'node14',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
+      exclude: ["aws-sdk"],
+      target: "node14",
+      define: { "require.resolve": undefined },
+      platform: "node",
       concurrency: 10,
     },
   },
